eeca986d3404311daf596e082e790158b9007a5a
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-06-30 23:36:31 +0530

    Update backend.tf

--- multi-cloud-terraform/envs/dev/aws/backend.tf
+++ multi-cloud-terraform/envs/dev/aws/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket"
+    bucket = "terraform-state-bucket23"
     key    = "dev/aws/terraform.tfstate"
     region = "us-east-1"
   }
