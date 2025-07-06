b217b59596c19c0fb75c853d6a72633972b904af
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-07-06 18:12:57 +0530

    Update main.tf

--- multi-cloud-terraform/modules/aws/storage/main.tf
+++ multi-cloud-terraform/modules/aws/storage/main.tf
@@ -7,3 +7,17 @@ resource "aws_s3_bucket" "this" {
     Name        = var.bucket_name
   }
 }
+
+
+# Keep existing
+#resource "aws_s3_bucket" "old_bucket" {
+#  bucket = "my-dev-s3-bucket-multicloud"
+#  # ... other config
+# 
+#}
+
+# Add new
+#resource "aws_s3_bucket" "new_bucket" {
+#  bucket = "my-dev-s3-bucket-multiclouds"
+#  # ... other config
+#}
