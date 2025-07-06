8e5f7c7c8400ef1d3f934e915c272ea6d4e06e6b
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-07-06 18:01:07 +0530

    Update main.tf

--- multi-cloud-terraform/aws/main.tf
+++ multi-cloud-terraform/aws/main.tf
@@ -1,5 +1,19 @@
+
+# multi-cloud-terraform/aws/main.tf
+
+# S3 Storage Module
 module "s3_storage" {
   source      = "../modules/aws/storage"
   bucket_name = var.bucket_name
   environment = var.environment
 }
+
+# EC2 Instance Module
+module "ec2_instance" {
+  source        = "../modules/aws/ec2"
+  instance_name = var.instance_name
+  instance_type = var.instance_type
+  environment   = var.environment
+  aws_region    = var.aws_region
+}
+
