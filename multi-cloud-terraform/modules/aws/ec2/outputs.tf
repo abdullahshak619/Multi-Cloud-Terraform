516d4ae5aaa739cb87eaf963d8e883bf28c34912
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-07-06 18:00:30 +0530

    Create outputs.tf

---
+++ multi-cloud-terraform/modules/aws/ec2/outputs.tf
@@ -0,0 +1,21 @@
+# modules/aws/ec2/outputs.tf
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.web.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
+output "instance_public_dns" {
+  description = "Public DNS name of the EC2 instance"
+  value       = aws_instance.web.public_dns
+}
+
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = aws_security_group.web_sg.id
+}
