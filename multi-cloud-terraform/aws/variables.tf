ef9f9372cd3b2a2b86c092d8fffd4a5d25311f2e
abdullahshak619 <90182619+abdullahshak619@users.noreply.github.com>
2025-07-06 18:01:35 +0530

    Update variables.tf

--- multi-cloud-terraform/aws/variables.tf
+++ multi-cloud-terraform/aws/variables.tf
@@ -1,7 +1,30 @@
+# multi-cloud-terraform/aws/variables.tf
+
+# S3 Variables
 variable "bucket_name" {
-  type = string
+  description = "Name of the S3 bucket"
+  type        = string
 }
 
 variable "environment" {
-  type = string
+  description = "Environment name"
+  type        = string
+}
+
+# EC2 Variables
+variable "instance_name" {
+  description = "Name of the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
